51f5d1cc408e1cabf17fe43259730910a98bd205
Liam <lgaskell@freewebstore.com>
2022-10-20 17:32:22 +0100

    feat: add MW test method

---
+++ MW.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+
+namespace CSharp_CODAPI
+{
+    public class MW
+    {
+        public async Task<BaseAPIResponse?> fullData(string gamertag, Platforms platform)
+        {
+            var lookupType = "gamer";
+
+            if (platform.Equals(Platforms.Steam)) throw new Exception(generics.STEAM_UNSUPPORTED);
+            if (platform == Platforms.Battlenet || platform == Platforms.Activision || platform == Platforms.Uno)
+                gamertag = HttpUtility.UrlEncode(gamertag);
+
+            if (platform.Equals(Platforms.Uno)) lookupType = "id";
+            if (platform.Equals(Platforms.Uno) || platform.Equals(Platforms.Activision))
+                platform = Platforms.Uno;
+
+            return await Http.sendRequest<BaseAPIResponse>($"/stats/cod/v1/title/mw/platform/{platform.ToString().ToLower()}/{lookupType}/{gamertag}/profile/type/mp");
+        }
+    }
+}
---
+++ Types.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization;
+
+namespace CSharp_CODAPI
+{
+    public class generics
+    {
+        public const string STEAM_UNSUPPORTED = "Steam platform not supported by this game. Try `battle` instead.";
+    }
+
+    public class BaseAPIResponse
+    {
+        public string? status { get; set; }
+        public object? data { get; set; }    
+    }
+
+    public enum Platforms : int
+    {
+        [EnumMember(Value = "all")]
+        All,
+        [EnumMember(Value = "acti")]
+        Activision,
+        [EnumMember(Value = "battle")]
+        Battlenet,
+        [EnumMember(Value = "psn")]
+        PSN,
+        [EnumMember(Value = "steam")]
+        Steam,
+        [EnumMember(Value = "uno")]
+        Uno,
+        [EnumMember(Value = "xbl")]
+        XBOX
+    }
+}
